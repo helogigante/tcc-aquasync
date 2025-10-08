6c5af43f286e34116c762d28de53e6b63a54587a
sc5402 <84030287+sc5402@users.noreply.github.com>
2025-10-08 18:47:41 -0300

    traduzindo parametros do mUsuario para ingles

--- api/models/usuarios.php
+++ api/models/usuarios.php
@@ -9,31 +9,31 @@
             $this->conn = $db;
         }
         function read(){
-            $query = "SELECT id_usuario, email, nome, telefone, senha from $this->table_name where id_usuario = :user_id";
+            $query = "SELECT id_usuario, email, nome, telefone from $this->table_name where id_usuario = :user_id";
             $stmt = $this->conn->prepare($query);
             $stmt->bindParam(":user_id",$this->user_id);
             return $stmt->execute();
         }
         function create(){
             $query = "insert into $this->table_nome (id_usuario, email, nome, telefone, senha) values
-            (:user_id, ':email', ':nome', ':telefone', ':senha')";
+            (:user_id, ':email', ':name', ':phone', ':password')";
             $stmt = $this->conn->prepare($query);
             //$this->senha = password_hash($this->senha, PASSWORD_BCRYPT);
             $stmt->bindParam(":user_id",$this->user_id);
             $stmt->bindParam(":email",$this->email);
-            $stmt->bindParam(":nome",$this->nome);
-            $stmt->bindParam(":telefone",$this->telefone);
-            $stmt->bindParam(":senha",$this->senha);
+            $stmt->bindParam(":name",$this->name);
+            $stmt->bindParam(":phone",$this->phone);
+            $stmt->bindParam(":password",$this->password);
             return $stmt->execute();
         }
         function update(){
-            $query ="update $this->table_name set email = ':email', nome = ':nome', telefone = ':telefone', senha = ':senha' where id_usuario = :user_id";
+            $query ="update $this->table_name set email = ':email', nome = ':name', telefone = ':phone', senha = ':password' where id_usuario = :user_id";
             $stmt = $this->conn->prepare($query);
             $stmt->bindParam(":user_id",$this->user_id);
             $stmt->bindParam(":email",$this->email);
-            $stmt->bindParam(":nome",$this->nome);
-            $stmt->bindParam(":telefone",$this->telefone);
-            $stmt->bindParam(":senha",$this->senha);
+            $stmt->bindParam(":name",$this->name);
+            $stmt->bindParam(":phone",$this->phone);
+            $stmt->bindParam(":password",$this->password);
             return $stmt->execute();
         }
         function delete(){

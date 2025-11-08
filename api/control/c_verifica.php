1ef57dbdf71c0a49a7602480d4ee17e719c0157d
sc5402 <84030287+sc5402@users.noreply.github.com>
2025-11-08 15:52:26 -0300

    Verificação funcionando

--- api/control/c_verifica.php
+++ api/control/c_verifica.php
@@ -1,9 +1,15 @@
 <?php
+    //ob_start();
+    //error_reporting(0);
+    //sheader_remove();
+
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json");
     header("Access-Control-Allow-Methods: GET");
     header("Access-Control-Max-Age: 6000"); 
     header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+    ob_clean();
 
     require_once '../config/database.php';
     require_once '../models/verifica.php';
@@ -14,9 +20,23 @@
 
     $method = $_SERVER['REQUEST_METHOD'];//supervariável global "$_XXX"
 
-    switch($method) {
-        case 'GET':
-           
-        break;
+    if(isset($_GET['sensor'])){ 
+        $verifica->sensor = $_GET['sensor'];
+        if($verifica->verificar()){
+            $verifica_arr = array (
+                "ex_daily"=>$verifica->ex_daily,
+                "ex_monthly"=>$verifica->ex_monthly,
+                "sus_24h"=>$verifica->sus_24h,
+                "sus_closed"=>$verifica->sus_closed
+            );             
+            http_response_code(200);
+            echo json_encode($verifica_arr);
+        } else {
+            http_response_code(404);
+            echo json_encode(array("message"=>"Não foi possível fazer a leitura dos dados."));       
+        }
+    } else {
+        http_response_code(404);
+        echo json_encode(array("message"=>"Parâmetros não foram recebidos.")); 
     }
 ?>
--- api/models/verifica.php
+++ api/models/verifica.php
@@ -3,13 +3,114 @@
         private $conn;
         private $table_name = "leituras";
     
-        public $sensor;
+        public $sensor, $report, $ex_daily, $ex_monthly, $sus_24h, $sus_closed;
 
         public function __construct($db) {
             $this->conn = $db;
         }
-        function verificar(){
 
+        function verificar(){
+            $number_zero = 0;
+            $status = array();
+
+            $this->ex_daily = false;
+            $this->ex_monthly = false;
+            $this->sus_closed = false;
+            $this->sus_24h = false;
+            //ultrapassar a média de consumo diário dos últimos 10 dias a partir de 10%
+            $query = "SELECT 
+                        (SELECT AVG(m) AS average_consumption
+                            FROM (
+                                SELECT SUM(valor_leitura) AS m
+                                FROM $this->table_name 
+                                WHERE id_sensor = :sensor
+                                AND DATE(dt_hr_leitura) BETWEEN DATE_SUB(CURDATE(), INTERVAL 11 DAY) 
+                                                            AND DATE_SUB(CURDATE(), INTERVAL 1 DAY)
+                                GROUP BY DATE(dt_hr_leitura)
+                            ) AS ds
+                        ) AS previous_days,
+                        (SELECT SUM(valor_leitura) 
+                        FROM $this->table_name 
+                        WHERE id_sensor = :sensor AND DATE(dt_hr_leitura) = CURDATE()
+                        ) AS actual_day;";
+
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":sensor", $this->sensor);
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if($row !== null && $row['previous_days'] !== null && $row['actual_day'] !== null) {//CONFERIR SE É NULL NOS DOIS ULTIMOS PARAMETROS
+                $previous_days = (float)$row['previous_days'];
+                $actual_day = (float)$row['actual_day'];
+                
+                if($previous_days > 0 && $actual_day * 1.1 > $previous_days) {
+                    $this->ex_daily = true;
+                }
+            }
+            //ultrapassar o consumo total do mês anterior em 18 dias ou menos
+            if(number_format(date("d")) > 18){
+                $query = "SELECT 
+                            (SELECT SUM(valor_leitura) 
+                            FROM $this->table_name 
+                            WHERE id_sensor = :sensor
+                                AND YEAR(dt_hr_leitura) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
+                                AND MONTH(dt_hr_leitura) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
+                            ) AS lastmonth,
+                            (SELECT SUM(valor_leitura) 
+                            FROM $this->table_name  
+                            WHERE id_sensor = :sensor
+                                AND MONTH(dt_hr_leitura) = month(NOW())
+                            ) AS actualmonth;";
+
+                $stmt = $this->conn->prepare($query);
+                $stmt->bindParam(":sensor", $this->sensor);
+                $stmt->execute();
+
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if($row !== null && $row['lastmonth'] !== null && $row['actualmonth'] !== null) {//CONFERIR SE É NULL NOS DOIS ULTIMOS PARAMETROS
+                    $this->ex_monthly = ((float)$row['actualmonth'] > (float)$row['lastmonth']);
+                }
+            }
+            
+            //O registro (tubulação) estiver marcado como “fechado”
+            $query = "SELECT valor_leitura
+                        FROM $this->table_name AS l
+                        JOIN sensor as s ON s.id_sensor = l.id_sensor
+                        WHERE l.id_sensor = :sensor 
+                        AND estado_registro = :r_state
+                        AND valor_leitura <> :r_value
+                        AND dt_hr_leitura - second(dt_hr_leitura) = NOW() - second(NOW());";
+
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":sensor", $this->sensor);
+            $stmt->bindParam(":r_state", $this->number_zero);
+            $stmt->bindParam(":r_value", $this->number_zero);
+
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $this->sus_closed = ($row !== null);
+            
+            //Houver vazão ininterrupta por mais de 24 horas 
+            $query = "SELECT valor_leitura
+                        FROM $this->table_name
+                        WHERE id_sensor = :sensor 
+                            AND dt_hr_leitura >= DATE_SUB(NOW(), INTERVAL 24 HOUR)
+                            AND valor_leitura > :r_value;";
+
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":sensor", $this->sensor);
+            $stmt->bindParam(":r_value", $this->number_zero);
+
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->sus_24h = ($row !== null && $row['total'] > 0);
+
+            return !in_array(false, $status);     
         }
     }
 ?>
